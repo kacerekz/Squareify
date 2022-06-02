5fc49893a639ce7c5a891df03aaa27836205ff57
kacerekz <adrianagreenrain@live.com>
2022-06-02 16:25:52 +0200

    Add project files.

---
+++ Squareify/ImageUtil.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Squareify
+{
+    class ImageUtil
+    {
+        /// <summary>
+        /// Color indices
+        /// </summary>
+        public const int R = 0, G = 1, B = 2;
+
+        /// <summary>
+        /// Converts a RGB byte array to a bitmap
+        /// </summary>
+        /// <param name="data">Source array</param>
+        /// <returns>Bitmap</returns>
+        public static Bitmap RgbToBitmap(byte[][][] data)
+        {
+            int h = data[0].Length;
+            int w = data[0][0].Length;
+
+            Bitmap bmp = new Bitmap(w, h);
+            BitmapData bmpdata = bmp.LockBits(new Rectangle(0, 0, w, h), ImageLockMode.WriteOnly, PixelFormat.Format32bppArgb);
+
+            int bytes = bmpdata.Stride * bmpdata.Height;
+            byte[] buffer = new byte[bytes];
+
+            int index = 0;
+
+            for (int i = 0; i < h; i++)
+            {
+                for (int j = 0; j < w; j++)
+                {
+                    buffer[index] = data[B][i][j];
+                    buffer[index + 1] = data[G][i][j];
+                    buffer[index + 2] = data[R][i][j];
+                    buffer[index + 3] = 255;
+                    index += 4;
+                }
+            }
+
+            Marshal.Copy(buffer, 0, bmpdata.Scan0, bytes);
+            bmp.UnlockBits(bmpdata);
+
+            return bmp;
+        }
+
+        /// <summary>
+        /// Converts a bitmap to an RGB byte array
+        /// </summary>
+        /// <param name="bitmap">Source image</param>
+        /// <returns>RGB byte array</returns>
+        public static byte[][][] BitmapToRgb(Bitmap bitmap)
+        {
+            int w = bitmap.Width;
+            int h = bitmap.Height;
+            byte[][][] rgb = new byte[3][][];
+
+            rgb[R] = new byte[h][];
+            rgb[G] = new byte[h][];
+            rgb[B] = new byte[h][];
+
+            Parallel.For(0, h, i =>
+            {
+                rgb[R][i] = new byte[w];
+                rgb[G][i] = new byte[w];
+                rgb[B][i] = new byte[w];
+            });
+
+            for (int i = 0; i < h; i++)
+            {
+                for (int j = 0; j < w; j++)
+                {
+                    var c = bitmap.GetPixel(j, i);
+                    rgb[R][i][j] = c.R;
+                    rgb[G][i][j] = c.G;
+                    rgb[B][i][j] = c.B;
+                }
+            }
+
+            return rgb;
+        }
+    }
+}
---
+++ Squareify/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.IO;
+using System.Drawing;
+
+namespace Squareify
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1) goto exit;
+            if (!Directory.Exists(args[0])) goto exit;
+
+            var dir = args[0];
+            var files = Directory.EnumerateFiles(dir);
+
+            foreach (var file in files)
+            {
+                var b = new Bitmap(file);
+                var brgb = ImageUtil.BitmapToRgb(b);
+                int size = Math.Max(b.Width, b.Height);
+                Color avgcol = AvgColor(brgb);
+
+                var nrgb = MakeImage(size, avgcol);
+                nrgb = FillImage(nrgb, brgb);
+                var nb = ImageUtil.RgbToBitmap(nrgb);
+                b.Dispose();
+                File.Delete(file);
+                nb.Save(file);
+            }
+            return;
+
+        exit:
+            Console.WriteLine("Launch with path to directory.");
+            return;
+        }
+
+        private static byte[][][] FillImage(byte[][][] nrgb, byte[][][] brgb)
+        {
+            var brgbA = brgb[0].Length;
+            var brgbB = brgb[0][0].Length;
+            var size = nrgb[0].Length;
+
+            var aoff = size - brgbA;
+            var boff = size - brgbB;
+            aoff /= 2;
+            boff /= 2;
+
+            for (int i = 0; i < brgbA; i++)
+            {
+                for (int j = 0; j < brgbB; j++)
+                {
+                    nrgb[0][aoff+i][boff+j] = brgb[0][i][j];
+                    nrgb[1][aoff+i][boff+j] = brgb[1][i][j];
+                    nrgb[2][aoff+i][boff+j] = brgb[2][i][j];
+                }
+            }
+
+            return nrgb;
+        }
+
+        private static Color AvgColor(byte[][][] brgb)
+        {
+            int avgr = 0;
+            int avgg = 0;
+            int avgb = 0;
+            int n = 0;
+
+            for (int i = 0; i < brgb[0].Length; i++)
+                for (int j = 0; j < brgb[0][0].Length; j++)
+                {
+                    avgr += brgb[0][i][j];
+                    avgg += brgb[1][i][j];
+                    avgb += brgb[2][i][j];
+                    n++;
+                }
+
+            avgr /= n;
+            avgg /= n;
+            avgb /= n;
+
+            avgr = Math.Min(avgr, 255);
+            avgg = Math.Min(avgg, 255);
+            avgb = Math.Min(avgb, 255);
+
+            avgr = Math.Max(avgr, 0);
+            avgg = Math.Max(avgg, 0);
+            avgb = Math.Max(avgb, 0);
+
+            return Color.FromArgb(avgr, avgg, avgb);
+        }
+
+        private static byte[][][] MakeImage(int size, Color avgcol)
+        {
+            byte[][][] n = new byte[3][][];
+
+            n[0] = new byte[size][];
+            n[1] = new byte[size][];
+            n[2] = new byte[size][];
+
+
+            for (int i = 0; i < size; i++)
+            {
+                n[0][i] = new byte[size];
+                n[1][i] = new byte[size];
+                n[2][i] = new byte[size];
+            }
+
+            for (int i = 0; i < size; i++) 
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    n[0][i][j] = avgcol.R;
+                    n[1][i][j] = avgcol.G;
+                    n[2][i][j] = avgcol.B;
+                }
+            }
+            
+            return n;
+        }
+    }
+}
